e7d7fd582f7ddee01ff9dbe50925a77b350668de
LuanTran <tranluanqqq@gmail.com>
2025-03-08 21:38:50 +0700

    feat(easy): 03/08/2025 leetcode

---
+++ easy/minimumRecolors.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} blocks
+ * @param {number} k
+ * @return {number}
+ */
+const minimumRecolors = function (blocks, k) {
+  let n = blocks.length;
+  let w = 0;
+
+  for (let i = 0; i < k; i++) {
+    if (blocks[i] === "W") w++;
+  }
+
+  let ans = w;
+
+  for (let i = k; i < n; i++) {
+    if (blocks[i] === "W") w++;
+    if (blocks[i - k] === "W") w--;
+    ans = Math.min(ans, w);
+  }
+
+  return ans;
+};
